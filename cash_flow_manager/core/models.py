af72588e0fdf3f05afc7433dc3e937bba614e7be
tomerismeshitbai <tomerismesitbaj@gmail.com>
2025-04-19 14:42:14 +0500

    adding models.py for core app

--- cash_flow_manager/core/models.py
+++ cash_flow_manager/core/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+class Status(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class TransactionType(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    transaction_type = models.ForeignKey(TransactionType, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Subcategory(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class CashFlow(models.Model):
+    created_at = models.DateField(auto_now_add=True)
+    custom_date = models.DateField(null=True, blank=True)
+    status = models.ForeignKey(Status, on_delete=models.PROTECT)
+    transaction_type = models.ForeignKey(TransactionType, on_delete=models.PROTECT)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    subcategory = models.ForeignKey(Subcategory, on_delete=models.PROTECT)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    comment = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.custom_date or self.created_at} | {self.amount} ₽"
